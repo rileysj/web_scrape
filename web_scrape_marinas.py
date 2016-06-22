4ba28a879c939c211a75d9b9e178fb8e2fdcb516
Steve <Steve@Macintosh.local>
2016-06-22 14:22:55 -0400

    cmon mike

--- web_scrape_marinas.py
+++ web_scrape_marinas.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+import logging
 
-BASE_URL = "http://www.chicagoreader.com"
+BASE_URL = "https://marinas.com/browse/marinas/us/TN/"
 
 def get_category_links(section_url):
-    html = urlopen(section_url).read()
+    html = urlopen(BASE_URL).read()
+    console.log("please print\n")
     soup = BeautifulSoup(html, "lxml")
     boccat = soup.find("dl", "boccat")
     category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
