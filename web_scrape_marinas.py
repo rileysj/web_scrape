5fdbdf19383fca9e471c7b9bbd3af3da6b1c9296
rileysj <rileysj@users.noreply.github.com>
2016-06-22 15:08:08 -0400

    Update web_scrape_marinas.py

--- web_scrape_marinas.py
+++ web_scrape_marinas.py
@@ -1,13 +1,60 @@
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
-import logging
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <curl/curlbuild.h>
+include "sstream"
+include "iostream"
+using namespace std;
 
-BASE_URL = "https://marinas.com/browse/marinas/us/TN/"
+class HTTPDownloader {
+public:
+    HTTPDownloader();
+    ~HTTPDownloader();
+    /**
+     * Download a file using HTTP GET and store in in a std::string
+     * @param url The URL to download
+     * @return The download result
+     */
+    std::string download(const std::string& url);
+private:
+    void* curl;
+};
 
-def get_category_links(section_url):
-    html = urlopen(BASE_URL).read()
-    console.log("please print\n")
-    soup = BeautifulSoup(html, "lxml")
-    boccat = soup.find("dl", "boccat")
-    category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
-    return category_links
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
+    string data((const char*) ptr, (size_t) size * nmemb);
+    *((stringstream*) stream) << data << endl;
+    return size * nmemb;
+}
+
+HTTPDownloader::HTTPDownloader() {
+    curl = curl_easy_init();
+}
+
+HTTPDownloader::~HTTPDownloader() {
+    curl_easy_cleanup(curl);
+}
+
+string HTTPDownloader::download(const std::string& url) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    /* example.com is redirected, so we tell libcurl to follow redirection */
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp causes uninitialized stack frame" bug
+    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+    std::stringstream out;
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+    /* Perform the request, res will get the return code */
+    CURLcode res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    }
+    return out.str();
+}
+
+int main(int argc, char** argv) {
+    HTTPDownloader downloader;
+    std::string content = downloader.download("https://stackoverflow.com");
+    std::cout << content << std::endl;
+}
